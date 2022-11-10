d95eaaee4e982f3009bcfc15fac34cc055c56b69
joonsooan <joonsooan@gmail.com>
2022-11-10 14:52:29 +0900

    making MOMENTUM

---
+++ js/greetings.js
@@ -0,0 +1,33 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const SUBMIT = "submit";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings();
+}
+
+loginForm.addEventListener(SUBMIT, onLoginSubmit);
+
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener(SUBMIT, onLoginSubmit);
+} else {
+  paintGreetings();
+}
+console.log(savedUsername);
